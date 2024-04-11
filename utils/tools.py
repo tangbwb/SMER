b6f299c33e9dc27a8a8794af493e609963c9655a
Bony Tang <67948196+tangbwb@users.noreply.github.com>
2024-04-11 15:10:02 +0800

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -1,15 +1,4 @@
 import os
-import numpy as np
-import torch
-
-def same_seeds(seed):
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
 
 def get_project_path():
     """得到项目路径"""
